4c3971d5df82088310177644042573f0aba8f2e2
99btyler <99tylerberinger@gmail.com>
2022-01-23 01:49:20 -0500

    Added print statements, improved readability

--- src/main.py
+++ src/main.py
@@ -7,41 +7,50 @@ def ask(question, requires_number=False):
 	return int(answer) if requires_number else answer
 
 
+print("\n---- ratio-helper ----")
+
+# GET ALL DATA
 things = {}
-amount_total = 0
-
 while True:
-
-	name = ask("name")
-	amount = ask("amount", True)
-	things[name] = amount
-	amount_total += amount
-
-	if ask("DONE?(press y)") == "y":
+	thing_name = ask("\nthing_name")
+	thing_amount = ask("thing_amount", True)
+	things[thing_name] = thing_amount
+	if ask("\nDONE?(press y)") == "y":
 		break
 
-for key in things:
+print("")
+for key in things.keys():
 	print(f"{key}({things[key]})")
 
-
 things_to_isolate = []
-amount_isolated = 0
-
 while True:
-
-	thing_to_isolate = ask("thing_to_isolate")
-	while thing_to_isolate not in things.keys():
-		thing_to_isolate = ask("thing_to_isolate") # asking again
-	things_to_isolate.append(thing_to_isolate)
-	amount_isolated += things[thing_to_isolate]
-
-	if ask("DONE?(press y)") == "y":
+	thing_to_isolate_name = ask("\nthing_to_isolate_name")
+	while thing_to_isolate_name not in things.keys():
+		thing_to_isolate_name = ask("thing_to_isolate_name") # asking again
+	if thing_to_isolate_name not in things_to_isolate:
+		things_to_isolate.append(thing_to_isolate_name)
+	if ask("\nDONE?(press y)") == "y":
 		break
 
+# EXTRACT SPECIFIC DATA
+things_amount = 0
+for key in things.keys():
+	things_amount += things[key]
 
-isolated_name = "".join(things_to_isolate)
-amount_non_isolated = amount_total-amount_isolated
+things_to_isolate_name = "".join(things_to_isolate)
 
-print(f"{amount_isolated} {isolated_name} vs. {amount_non_isolated} non-{isolated_name}")
-print(f"1 {isolated_name} = {amount_non_isolated/amount_isolated} non-{isolated_name}")
-print(f"{round((amount_isolated/amount_total)*100)}% {isolated_name}, {round((amount_non_isolated/amount_total)*100)}% non-{isolated_name}")
+things_to_isolate_amount = 0
+for key in things.keys():
+	if key in things_to_isolate:
+		things_to_isolate_amount += things[key]
+
+things_not_isolated_amount = things_amount-things_to_isolate_amount
+
+# FINISH
+print(f"\n{things_to_isolate_amount} {things_to_isolate_name} vs. {things_not_isolated_amount} non-{things_to_isolate_name}")
+print(f"1 {things_to_isolate_name} = {round(things_not_isolated_amount/things_to_isolate_amount, 2)} non-{things_to_isolate_name}")
+print(f"{round((things_to_isolate_amount/things_amount)*100)}% {things_to_isolate_name} vs. {round((things_not_isolated_amount/things_amount)*100)}% non-{thing_to_isolate_name}")
+
+
+print("\n----------------------\n")
+
